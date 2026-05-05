1370591bb4d6f84c4df92a9a1da49ec1faad9181
Hitesh BR <hiteshbr1511@gmail.com>
2026-05-05 09:23:17 +0530

    fixed duplicate import issue

--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,18 +1,16 @@
 import express from "express";
-import { addStudent, getStudents, saveDeviceToken } from "../controllers/studentController.js";
-import protect from "../middleware/authMiddleware.js";
-import { getStudents, createStudent, updateStudent } from "../controllers/studentController.js";
+import {
+  getStudents,
+  createStudent,
+  updateStudent
+} from "../controllers/studentController.js";
+
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-// Create student
-router.post("/", protect, addStudent);
-
-// Get all students
-router.get("/", protect, getStudents);
-router.post("/students", createStudent);
-
-// Save device token
-router.put("/69f48260b636e80bb5a40f5b/save-token", saveDeviceToken);
+router.get("/students", authMiddleware, getStudents);
+router.post("/students", authMiddleware, createStudent);
+router.put("/students/69f48260b636e80bb5a40f5b", authMiddleware, updateStudent);
 
 export default router;
